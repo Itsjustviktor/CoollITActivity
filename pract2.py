b745ff87732a684ec4cfe9a5f5ff28f5f0df8f1b
saakiys <saak4ik@gmail.com>
2021-03-30 19:36:31 +0300

    Практика №2 Вариант 14

---
+++ pract2.py
@@ -0,0 +1,43 @@
+import random as rnd
+
+
+##Вариант 14
+
+## задание 7
+def fibonacci(n):
+    if n in (1, 2):
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+i=1
+while (i <=13):
+    print (fibonacci(i)) ;
+    i+=1
+
+## задание 9
+try:
+    a = int(input("Введите первое целое число: "))
+    b = int(input("Введите второе целое число: "))
+    h = int(input("Напишите результат произведения: "))
+    res = a * b
+    if res == h:
+        print("Верно")
+    else:
+        print("Ошибка, правильный ответ: ", res)
+except:
+    print("Ошибка при вводе данных!")
+
+## задание 10
+def mass(n):
+    arr = []
+    for i in range(n):
+        arr.append(rnd.random())
+    a = (max(arr), min(arr))
+    return arr, a
+
+
+n = int(input("Введите размерность массива: "))
+mat, a = mass(n)
+print("Массив: ", mat)
+print("Макс. и Мин. значения в кортеже: ", a)
+
+

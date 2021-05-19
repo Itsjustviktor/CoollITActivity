726fe75eb67d1c2045f9f85291701df80483117f
saakiys <63611047+saakiys@users.noreply.github.com>
2021-05-19 19:02:15 +0300

    Добавлена нормализация данных

--- pract4/kNN.py
+++ pract4/kNN.py
@@ -1,8 +1,19 @@
 import numpy as np
 import math
+from sklearn.preprocessing import StandardScaler
 
 def k_nearest(X, k, obj):
     X_sub = X[:, 0:-1]
+    
+    # нормализация данных
+    scaler = StandardScaler()
+    scaler.fit(X_sub)
+    X_sub_Mean = scaler.mean_
+    X_sub_Std = np.std(X_sub, axis=0)
+    for i in range(len(obj)):
+        obj[i] = (obj[i] - (np.mean(X_sub, axis=0))[i]) / (np.std(X_sub, axis=0))[i]
+    for i in range(X_sub.shape[1]):
+        X_sub[:, i] = (X_sub[:, i] - X_sub_Mean[i]) / X_sub_Std[i]
 
     a = [dist(i, obj) for i in X_sub]
     h = np.argsort(a)

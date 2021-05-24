d934e73a3351c540a8fb7effb7b8f8a66cc52203
saakiys <saak4ik@gmail.com>
2021-05-24 17:12:31 +0300

    Исправленная

--- pract3/k_means.py
+++ pract3/k_means.py
@@ -1,13 +1,8 @@
 import numpy as np
 
-def dist(A, B):
-  # Для вычисления квадратного корня используйте функцию np.sqrt
-  r = np.sqrt(np.sum((A-B)**2))
-  return r
-
 
 # возвращает список индексов ближайших центров по каждой точке
-def class_of_each_point(X, centers):
+def class_of_each_point(X, centers, method):
   m = len(X)
   k = len(centers)
 
@@ -15,13 +10,13 @@ def class_of_each_point(X, centers):
   distances = np.zeros((m, k))
   for i in range(m):
     for j in range(k):
-      distances[i, j] = dist(centers[j], X[i])
+      distances[i, j] = method(centers[j], X[i])
 
   # поиск ближайшего центра для каждой точки
   return np.argmin(distances, axis=1)
 
 
-def kmeans(k, X):
+def kmeans(k, X, method):
   m = X.shape[0]  # количество строк в матрице X
   n = X.shape[1]  # количество столбцов в матрице X
   curr_iteration = prev_iteration = np.zeros(m)
@@ -29,10 +24,12 @@ def kmeans(k, X):
   centers =(np.max(X, axis=0) - np.min(X, axis=0)) * np.random.random((k, n)) + np.min(X, axis=0)
 
   # приписываем каждую точку к заданному классу
-  curr_iteration = class_of_each_point(X, centers)
+  curr_iteration = class_of_each_point(X, centers,method)
+
   while np.any(prev_iteration != curr_iteration):
+
     prev_iteration = curr_iteration
-    
+
     # вычисляем новые центры масс
     for i in range(k):
       sub_X = X[curr_iteration == i,:]
@@ -40,6 +37,6 @@ def kmeans(k, X):
         centers[i,:] = np.mean(sub_X, axis=0)
 
     # приписываем каждую точку к заданному классу
-    curr_iteration = class_of_each_point(X, centers)
+    curr_iteration = class_of_each_point(X, centers, method)
   
   return centers
--- pract3/run.py
+++ pract3/run.py
@@ -1,17 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from k_means import kmeans
+from pract3.k_means import kmeans
+
+
+
+# евклидово расстояние между двумя точками
+def dist(A, B):
+  r = np.sqrt(np.sum((A-B)**2))
+  return r
+
+# расстояние городских кварталов
+def dist_function(A, B):
+  r = np.sum(abs(A-B))
+  return r
+
+
 # исходные данные
 X = np.array([
   [4, 4],
   [3, 3],
   [5, 3],
-
   [5, 4],
   [2, 2]])
 
 # запуск кластеризации
-ans = kmeans(2, X)
+ans = kmeans(2, X, dist)
 [2, 3],
 [5, 5],
 [3, 2],
@@ -20,5 +33,20 @@ ans = kmeans(2, X)
 # отображение результатов
 print(ans)
 plt.plot(X[:,0], X[:,1], 'bx', ans[:,0], ans[:,1], 'r*', markersize=20)
+plt.grid()
+plt.show()
+
+
+
+# запуск кластеризации
+ans2 = kmeans(2, X, dist_function)
+[2, 3],
+[5, 5],
+[3, 2],
+[2, 4],
+[4, 5],
+# отображение результатов
+print(ans)
+plt.plot(X[:,0], X[:,1], 'bx', ans2[:,0], ans2[:,1], 'r*', markersize=20)
 plt.grid()
 plt.show()
